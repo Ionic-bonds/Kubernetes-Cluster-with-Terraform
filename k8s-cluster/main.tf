d02c389769810bfd6f809484ad744b19763fd7d3
Leonardwkwong <weikit.wong.2019@scis.smu.edu.sg>
2023-12-31 21:48:14 +0800

    Included data block for AZs, locals block and resource block

--- k8s-cluster/main.tf
+++ k8s-cluster/main.tf
@@ -8,3 +8,15 @@ provider "aws" {
   
 }
 
+data "aws_availability_zones" "available" {
+  
+}
+
+locals {
+  cluster_name = "eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length = 8 
+  special = false
+}
