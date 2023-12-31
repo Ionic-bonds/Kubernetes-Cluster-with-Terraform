872b9113b2cbeed14a82c08bb5ec776a2f945367
Leonardwkwong <weikit.wong.2019@scis.smu.edu.sg>
2023-12-31 21:42:15 +0800

    formatted terraform.tf

--- k8s-cluster/terraform.tf
+++ k8s-cluster/terraform.tf
@@ -1,29 +1,29 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 4.46.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.46.0"
     }
-  
+
 
     random = {
-        source = "hashicorp/random"
-        version = "~>3.4.3"
+      source  = "hashicorp/random"
+      version = "~>3.4.3"
     }
 
     tls = {
-        source = "hashicorp/tls"
-        version = "~> 4.0.4"
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
     }
 
     cloudinit = {
-        source = "hashicorp/cloudinit"
-        version = "~> 2.2.0"
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.2.0"
     }
 
     kubernetes = {
-        source = "hashicorp/kubernetes"
-        version = "~> 2.16.1"
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.16.1"
     }
   }
   required_version = "1.3"
